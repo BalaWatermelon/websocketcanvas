f5aec8f9681434be5707a4cd71603fd5423cd5bc
Jackson Lin <jackson1994y+git@gmail.com>
2018-11-04 14:01:07 -0500

    Room seperation and classmates

--- socketServer.py
+++ socketServer.py
@@ -8,25 +8,39 @@ import json
 
 buffer = []
 clients = []
-
+names = {}
 
 async def time(websocket, path):
     global clients
     global buffer
     clients.append(websocket)
     print('[*] A new client came in ^_^.')
-    print(clients)
+    print('Clients: {}'.format(len(clients)))
     try:
         while True:
-            point = await websocket.recv()
-            for client in clients:
-                await client.send(point)
+            msg = await websocket.recv()
+            m = json.loads(msg)
+            if m['msg'] =='draw':
+                for client in clients:
+                    await client.send(msg)
+            elif m['msg'] == 'newstudent':
+                if str(m['roomID']) in names:
+                    if m['name'] not in names[str(m['roomID'])]:
+                        names[str(m['roomID'])].append(m['name'])
+                else:
+                    names[str(m['roomID'])] = []
+                    names[str(m['roomID'])].append(m['name'])
+                r = {'msg':'stulist','data':names}
+                r = json.dumps(r)
+                for client in clients:
+                    await client.send(r)
     except websockets.exceptions.ConnectionClosed:
         clients.remove(websocket)
         print('[*] A client just leave ~owo~')
-        print(clients)
+        print('Clients: {}'.format(len(clients)))
 
-start_server = websockets.serve(time, '127.0.0.1', 9999)
+
+start_server = websockets.serve(time, '10.203.65.168', 9999)
 print('[*] Server is listening on 127.0.0.1:9999...')
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
